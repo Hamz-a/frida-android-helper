072676281f34d92df67673793635ad41c4475ae5
Hamza <dzcyberdev@gmail.com>
2021-09-28 20:32:49 +0200

    case insensitive ps

--- frida_android_helper/ps.py
+++ frida_android_helper/ps.py
@@ -8,5 +8,5 @@ def list_processes(search):
         eprint("📲 Device: {} ({})".format(get_device_model(device), device.get_serial_no()))
         frida_device = frida.get_device(device.get_serial_no())
         for app in frida_device.enumerate_applications():
-            if search in app.identifier or search in app.name:
+            if search.lower() in app.identifier.lower() or search.lower() in app.name.lower():
                 print("🌕 {} ({}) [{}]".format(app.name, app.identifier, app.pid))
