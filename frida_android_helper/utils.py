88dc16a189739f352580be6be5f87b5a3f431ae8
Hamza <dzcyberdev@gmail.com>
2023-12-30 19:03:35 +0100

    fix ip problem in fah proxy

--- frida_android_helper/utils.py
+++ frida_android_helper/utils.py
@@ -39,8 +39,19 @@ def perform_cmd(device: AdbDevice, command: str, root: bool = False, timeout: in
         return ""
 
 
-def get_ip_address():  # might need refactoring...
-    return socket.gethostbyname(socket.gethostname())
+# https://stackoverflow.com/a/28950776
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(("10.254.254.254", 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
 
 
 def get_device_model(device: AdbDevice):
